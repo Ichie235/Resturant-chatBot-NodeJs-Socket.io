c46143db5f915dc05ae3b50f2cd61d90a4d09682
ichie chinemrem richard <chinemeremichie@gmail.com>
2023-03-16 12:48:18 +0100

    first commit

---
+++ models/foodStore.js
@@ -0,0 +1,37 @@
+const products =[]
+
+class Foodstore {
+    constructor(id, title, price,currency) {
+        this.id = id;
+        this.title = title;
+        this.price = new Number(price);
+        this.currency = currency
+    }
+
+    save() {
+        products.push(this);
+    }
+    
+    // static findAll() {
+    //     return products;
+    // }
+
+    static findById(prodId) {
+        return products.filter(p => p.id == prodId);
+    }
+}
+
+
+const roastedCorn = new Foodstore("01",'Roasted Corn',780,'Naira')
+const bole = new Foodstore("02",'Spicy Bole',500,'Naira')
+const garri = new Foodstore("03",'Granulated Garri',1500,'Naira')
+const ewa = new Foodstore("04",'Ewa',2500,'Naira')
+const abacha = new Foodstore("05",'Abacha and kpomo',3500,'Naira')
+
+module.exports= {
+  roastedCorn,
+  bole,
+  garri,
+  ewa,
+  abacha
+}
